9358907ecd31c16176d066ec2a5bdda1f55d5d58
Ebn3bbas <abdo.abas777@gmail.com>
2024-06-04 16:14:10 +0300

    update

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,20 +1,15 @@
 import BlogList from "../components/BlogList";
-import Loading from "../components/Loading";
 import useFetch from "../hooks/uesFetch";
 const Home = () => {
   const {
     data: blogs,
-    isLoading,
     error,
   } = useFetch("http://localhost:8000/blogs");
-
   return (
     <div className="home">
       {error && <div>{error}</div>}
-      {isLoading && <Loading />}
       {blogs && <BlogList blogs={blogs} title="All Blogs" />}
     </div>
   );
 };
-
 export default Home;
